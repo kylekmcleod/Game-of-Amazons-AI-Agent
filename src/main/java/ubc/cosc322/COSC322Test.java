c6207bef11ee5a96a7c6cf81948d735aab56f9f1
kylekmcleod <kylekmcleod1@gmail.com>
2025-01-28 18:53:30 -0800

    Dhevan code

--- src/main/java/ubc/cosc322/COSC322Test.java
+++ src/main/java/ubc/cosc322/COSC322Test.java
@@ -1,107 +1,133 @@
-
 package ubc.cosc322;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import sfs2x.client.entities.Room;
 import ygraph.ai.smartfox.games.BaseGameGUI;
 import ygraph.ai.smartfox.games.GameClient;
+import ygraph.ai.smartfox.games.GameMessage;
 import ygraph.ai.smartfox.games.GamePlayer;
+import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
 
 /**
  * An example illustrating how to implement a GamePlayer
- * @author Yong Gao (yong.gao@ubc.ca)
- * Jan 5, 2021
- *
+ * Author: Modified by Assistant
  */
-public class COSC322Test extends GamePlayer{
+public class COSC322Test extends GamePlayer {
 
-    private GameClient gameClient = null; 
+    private GameClient gameClient = null;
     private BaseGameGUI gamegui = null;
-	
+
     private String userName = null;
     private String passwd = null;
- 
-	
+
     /**
      * The main method
      * @param args for name and passwd (current, any string would work)
      */
-    public static void main(String[] args) {				 
-    	COSC322Test player = new COSC322Test("test1", "test2");
-    	
-    	if(player.getGameGUI() == null) {
-    		player.Go();
-    	}
-    	else {
-    		BaseGameGUI.sys_setup();
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                public void run() {
-                	player.Go();
-                }
-            });
-    	}
+    public static void main(String[] args) {
+        COSC322Test player = new COSC322Test("cosc322", "cosc322");
+
+        if (player.getGameGUI() == null) {
+            player.Go();
+        } else {
+            BaseGameGUI.sys_setup();
+            java.awt.EventQueue.invokeLater(() -> player.Go());
+        }
     }
-	
+
     /**
-     * Any name and passwd 
-     * @param userName
-      * @param passwd
+     * Constructor for COSC322Test
+     * @param userName - Username for the game
+     * @param passwd - Password for the game
      */
     public COSC322Test(String userName, String passwd) {
-    	this.userName = userName;
-    	this.passwd = passwd;
-    	
-    	//To make a GUI-based player, create an instance of BaseGameGUI
-    	//and implement the method getGameGUI() accordingly
-    	//this.gamegui = new BaseGameGUI(this);
+        this.userName = userName;
+        this.passwd = passwd;
+        this.gamegui = new BaseGameGUI(this); // Initialize GUI instance
     }
- 
-
 
     @Override
     public void onLogin() {
-    	System.out.println("Congratualations!!! "
-    			+ "I am called because the server indicated that the login is successfully");
-    	System.out.println("The next step is to find a room and join it: "
-    			+ "the gameClient instance created in my constructor knows how!"); 
+        System.out.println("Login successful! Finding a room to join...");
+
+        userName = gameClient.getUserName(); // Set the user name
+        if (gamegui != null) {
+            gamegui.setRoomInformation(gameClient.getRoomList()); // Update room list in GUI
+        }
+
+        List<Room> rooms = gameClient.getRoomList();
+        System.out.println("Available Rooms:");
+        for (Room room : rooms) {
+            System.out.println("- " + room.getName());
+        }
+
+        if (!rooms.isEmpty()) {
+            Room roomToJoin = rooms.get(0);
+            System.out.println("Joining room: " + roomToJoin.getName());
+            gameClient.joinRoom(roomToJoin.getName());
+        }
     }
 
     @Override
     public boolean handleGameMessage(String messageType, Map<String, Object> msgDetails) {
-    	//This method will be called by the GameClient when it receives a game-related message
-    	//from the server.
-	
-    	//For a detailed description of the message types and format, 
-    	//see the method GamePlayer.handleGameMessage() in the game-client-api document. 
-    	    	
-    	return true;   	
+        System.out.println("Received game message: " + messageType);
+        System.out.println("Details: " + msgDetails);
+
+        switch (messageType) {
+            case GameMessage.GAME_STATE_BOARD:
+                if (gamegui != null) {
+                    gamegui.setGameState((ArrayList<Integer>) msgDetails.get(AmazonsGameMessage.GAME_STATE)); // Set the game board state in the GUIgame_S
+                }
+                break;
+
+            case GameMessage.GAME_ACTION_MOVE:
+                if (gamegui != null) {
+                    gamegui.updateGameState(msgDetails); // Update the game board in the GUI
+                }
+
+                // Here you can calculate your move based on the game state and send it to the server
+                // Example:
+                // Map<String, Object> myMove = calculateMyMove(msgDetails);
+                // gameClient.sendMoveMessage(myMove);
+                break;
+
+            default:
+                System.out.println("Unhandled game message type: " + messageType);
+        }
+
+        return true;
     }
-    
-    
+
     @Override
     public String userName() {
-    	return userName;
+        return userName;
     }
 
-	@Override
-	public GameClient getGameClient() {
-		// TODO Auto-generated method stub
-		return this.gameClient;
-	}
+    @Override
+    public GameClient getGameClient() {
+        return this.gameClient;
+    }
 
-	@Override
-	public BaseGameGUI getGameGUI() {
-		// TODO Auto-generated method stub
-		return  null;
-	}
+    @Override
+    public BaseGameGUI getGameGUI() {
+        return this.gamegui;
+    }
 
-	@Override
-	public void connect() {
-		// TODO Auto-generated method stub
-    	gameClient = new GameClient(userName, passwd, this);			
-	}
+    @Override
+    public void connect() {
+        gameClient = new GameClient(userName, passwd, this);
+    }
 
- 
-}//end of class
+    /**
+     * Example method to calculate your move (placeholder for implementation)
+     * @param msgDetails - Details of the current game state
+     * @return A map representing your move
+     */
+    private Map<String, Object> calculateMyMove(Map<String, Object> msgDetails) {
+        // Implement your move calculation logic here
+        return null;
+    }
+}
