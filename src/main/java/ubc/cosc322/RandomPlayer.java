cfc56b1b21b078dfad6739fc420d47edf40fb4a8
kylekmcleod <kylekmcleod1@gmail.com>
2025-03-04 14:13:01 -0800

    File structure organized & documentation

--- src/main/java/ubc/cosc322/RandomPlayer.java
+++ src/main/java/ubc/cosc322/RandomPlayer.java
@@ -1,23 +1,30 @@
 package ubc.cosc322;
 
-import java.util.ArrayList;
 import java.util.Map;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
+
 import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
 
-public class MoveCalculator {
-    
-    /**
-     * Calculate the next move for the game
-     * @return A map containing the move details
-     */
-    public Map<String, Object> calculateMove() {
+public class RandomPlayer extends BasePlayer {
+
+    private Random random;
+
+    public RandomPlayer(String userName, String passwd) {
+        super(userName, passwd);
+        this.random = new Random();
+    }
+
+    @Override
+    protected void processMove(Map<String, Object> msgDetails) {
         Map<String, Object> move = new HashMap<>();
         move.put(AmazonsGameMessage.QUEEN_POS_CURR, new ArrayList<>(List.of(10, 4)));
         move.put(AmazonsGameMessage.QUEEN_POS_NEXT, new ArrayList<>(List.of(9, 4)));
         move.put(AmazonsGameMessage.ARROW_POS, new ArrayList<>(List.of(9, 5)));
         
-        return move;
+        gamegui.updateGameState(move);
+        gameClient.sendMoveMessage(move);
     }
 }
