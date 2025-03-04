3716acc5412835cc28b9144aad64d983827d70cb
kylekmcleod <kylekmcleod1@gmail.com>
2025-03-04 14:02:05 -0800

    Main class with docs

---
+++ src/main/java/ubc/cosc322/Main.java
@@ -0,0 +1,22 @@
+package ubc.cosc322;
+
+import ygraph.ai.smartfox.games.BaseGameGUI;
+import ygraph.ai.smartfox.games.amazons.HumanPlayer;
+
+/*
+ * The main class that runs the game. It creates a random player and starts the game.
+ * Uncomment the line that creates a HumanPlayer object to play the game as a human.
+ */
+public class Main {
+    public static void main(String[] args) {
+        //HumanPlayer player = new HumanPlayer();
+        RandomPlayer player = new RandomPlayer("Player" + (int) (Math.random() * 10000), "2");
+
+        if (player.getGameGUI() == null) {
+            player.Go();
+        } else {
+            BaseGameGUI.sys_setup();
+            java.awt.EventQueue.invokeLater(player::Go);
+        }
+    }
+}
