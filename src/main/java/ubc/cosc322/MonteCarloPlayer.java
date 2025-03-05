f1c1d706903652abb4860519bf0a1e40ae6dffa1
kylekmcleod <kylekmcleod1@gmail.com>
2025-03-05 15:13:11 -0800

    Monte carlo comment

--- src/main/java/ubc/cosc322/MonteCarloPlayer.java
+++ src/main/java/ubc/cosc322/MonteCarloPlayer.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Random;
 import ygraph.ai.smartfox.games.amazons.AmazonsGameMessage;
 
-/* MonteCarloPlayer.java
+/* MonteCarloPlayer.java (incomplete)
  *
  * A monte carlo player that makes moves using the monte carlo tree search algorithm. It extends the BasePlayer class.
  * The processMove method is overridden to make moves using the monte carlo tree search algorithm.
